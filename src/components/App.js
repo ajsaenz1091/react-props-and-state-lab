4ff1df6d753dfeda88f494d19d63c72e3f67788d
Alberto <ajsaenz1091@gmail.com>
2021-05-28 16:22:58 -0400

    commit tree 52c1c63e869644093b3b8c8592b283c4c23b4b8f

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
 
   onChangeType = (e) => {
     e.persist();
+    // let type = e.target.value
     this.setState(prevState => ({
       ...prevState,
       filters: {
